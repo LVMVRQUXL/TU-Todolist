dee16e23517e0cb45737337946acb97cea320f33
Loïc Lamarque <loiclamarque777@gmail.com>
2020-07-01 12:09:53 +0200

    feature(api): [API-02] :sparkles: Implement task's creation

--- api/src/controllers/task.controller.js
+++ api/src/controllers/task.controller.js
@@ -1,5 +1,18 @@
-class TaskController {
+const TaskService = require('../services').TaskService;
 
+class TaskController {
+    /**
+     * Create a new task
+     *
+     * @param task {object}
+     *
+     * @returns {Promise<TaskDTO|undefined>}
+     */
+    async createTask(task) {
+        const createdTask = await TaskService.create(task);
+
+        return createdTask ? TaskService.mapToDTO(createdTask) : undefined;
+    }
 }
 
 module.exports = new TaskController();
